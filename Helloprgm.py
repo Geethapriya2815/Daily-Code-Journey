710f8ea31c3d80fd0c49e74e2aa1ea0bb3caa6cf
GEETHAPRIYA S L <s.l.geethapriya8888@gmail.com>
2025-01-28 20:52:00 +0530

    Create Helloprgm.py

---
+++ Helloprgm.py
@@ -0,0 +1,9 @@
+#Your task is to create a program that displays the message "Hello world" on the screen.
+#How will you set up your program to print this greeting clearly and correctly?
+
+
+def hello(str):
+  print(str)
+
+hello("Hello world")
+  
