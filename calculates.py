38766672adbc2659c67134ab9fbf48250da6ca53
GEETHAPRIYA S L <s.l.geethapriya8888@gmail.com>
2025-01-28 21:06:59 +0530

    Create calculates.py

---
+++ calculates.py
@@ -0,0 +1,8 @@
+#You have two numbers, and your task is to write a program that reads these numbers and calculates their sum.
+#How will you make sure the program adds them together correctly and shows the result?
+
+def add(a,b):
+  sums = a + b
+  return sums
+adds = add(188,233)
+print(adds)
