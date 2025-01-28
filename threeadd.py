0195dd8fdec70a988c8d1f5b061a8626f567c487
GEETHAPRIYA S L <s.l.geethapriya8888@gmail.com>
2025-01-28 21:10:36 +0530

    Create threeadd.py

---
+++ threeadd.py
@@ -0,0 +1,8 @@
+#You have three numbers, and your challenge is to write a program that reads these numbers and computes their total sum. 
+#How will you ensure your program adds them all together to give the final result?
+
+def three_no_sum(a,b,c):
+  sums = a + b + c
+  return sums
+adds = three_no_sum(24,24,24)
+print("final result: ",adds)
